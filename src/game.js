ad184099af3c894b10d241308c30d2221b9b8abb
Tolik22869 <ggamer556@mail.ru>
2021-12-02 22:12:01 +0300

    add move piece

---
+++ src/game.js
@@ -0,0 +1,84 @@
+export default class Game {
+  score = 0;
+  lines = 0;
+  level = 0;
+  playfield = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+  activePiece = {
+    x: 0,
+    y: 0,
+    blocks: [
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0],
+    ],
+  };
+
+  movePieceLeft() {
+    if (!this.isPieceOutOfBounds()) {
+      this.activePiece.x -= 1;
+    }
+  }
+
+  movePieceRight() {
+    if (!this.isPieceOutOfBounds()) {
+      this.activePiece.x += 1;
+    }
+  }
+
+  movePieceDown() {
+    if (!this.isPieceOutOfBounds()) {
+      this.activePiece.y += 1;
+    }
+  }
+
+  hasCollision() {
+    const playfield = this.playfield;
+    const { x: pieceX, y: pieceY, blocks } = this.activePiece;
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (
+          blocks[y][x] &&
+          ((playfield[pieceY + y] === undefined ||
+            playfield[pieceY + y][pieceX + x] === undefined) ||
+            playfield[pieceY + y][pieceX + x])
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  lockPiece() {
+    const { x: pieceX, y: pieceY, blocks } = this.activePiece;
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+        }
+      }
+    }
+  }
+}
